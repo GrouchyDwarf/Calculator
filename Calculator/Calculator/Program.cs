6ff01330c2ad63db846ea69a51cae290f8ed99d9
Ivan Yeremenko <ivanyeremenko3@gmail.com>
2021-01-11 15:51:21 +0200

    Task Main

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
+
 namespace Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var consoleInteractive = new ConsoleInteractive();
             var rpn = new RPN(consoleInteractive);
             var script = new Script(consoleInteractive);
-            script.RunScript(rpn);
+            await script.RunScript(rpn);
         }
     }
 }
